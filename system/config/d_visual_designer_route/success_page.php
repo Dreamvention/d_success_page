5035d54efad48f110ba6d3936b5f66e1af69ee49
Dmitriy Sokol <php.drvn@gmail.com>
2018-03-15 18:08:32 +0200

    chage event turn on logic

--- system/config/d_visual_designer_route/success_page.php
+++ system/config/d_visual_designer_route/success_page.php
@@ -17,7 +17,6 @@ $_['frontend_route']    = 'checkout/success';
 $_['edit_route']        = 'extension/d_visual_designer/designer/saveInformation';
 //События необходимые для работы данного route
 $_['events']            = array(
-    'catalog/view/common/success/before' => 'extension/event/d_success_page/view_checkout_success',
     'admin/view/extension/module/d_success_page/after' => 'extension/event/d_success_page/view_extension_module_d_success_page_after',
     'admin/model/extension/module/d_success_page/addInformation/after' => 'extension/event/d_success_page/model_extension_module_d_success_page_addInformation_after'
 );
